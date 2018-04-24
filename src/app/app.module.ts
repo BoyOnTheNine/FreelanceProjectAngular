4c93a8e95dbfa83f027086b11b2b045d9334175f
Artem Slizhevskiy <mykew98@gmail.com>
2018-04-24 19:26:17 +0300

    Added Bootstrap CSS and NgBootstrap modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 
 import {RouterModule} from '@angular/router';
@@ -21,6 +24,7 @@ import { RegisterComponent } from './register/register.component';
 
 @NgModule({
     imports:     [
+        NgbModule.forRoot(),
         BrowserModule,
         FormsModule,
         RouterModule.forRoot([
