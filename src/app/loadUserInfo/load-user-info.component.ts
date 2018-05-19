631af1d3694c970deb35c27418479a6da2bc532c
Artem Slizhevskiy <mykew98@gmail.com>
2018-05-19 20:19:11 +0300

    Added user info loading page

--- src/app/loadUserInfo/load-user-info.component.ts
+++ src/app/loadUserInfo/load-user-info.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../shared/user';
 import {Offer} from '../shared/offer';
 import {OfferService} from '../offer/offer.service';
+import { UserService } from '../user-service/user.service';
+import { AuthenticationService } from '../authorise/authentication.service';
 
 @Component({
     selector: "load",
@@ -12,9 +14,14 @@ export class LoadUserComponent implements OnInit{
     @Input() user: User;
     offers: Offer[];
 
-    constructor(private offerService : OfferService) {}
+    constructor(private offerService : OfferService, 
+        private userService: UserService, 
+        private authService: AuthenticationService) {}
 
     ngOnInit(){
+        this.userService.GetUserByLogin("userlogin").subscribe(data => {
+            this.user = data;
+            console.log('this.user = ' + this.user)});
       this.offers =  this.offerService.getUserOffers(this.user);
     }
 }
