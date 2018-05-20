0b17e7451e9e1f512277a9d6af9c273d5879dee4
Artem Slizhevskiy <mykew98@gmail.com>
2018-05-20 18:31:19 +0300

    Removed Http module. Offer cards modification in user info page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HttpModule } from '@angular/http'; 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
@@ -36,7 +35,6 @@ import { AlertService } from './alert-service/alert.service';
         NgbModule.forRoot(),
         BrowserModule,
         FormsModule,
-        HttpModule,
         HttpClientModule,
         RouterModule.forRoot([
             {path: 'home',component: HomeComponent},
