a59e4516608335a67368443051256c95dcaafa44
DESKTOP-484ARDL\matvej <matvejk97@gmail.com>
2018-05-22 01:29:18 +0300

    NO IDIEAS ABOUT THIS COMMIT NAME:)

--- src/app/offer-detail/offer-deatil.component.ts
+++ src/app/offer-detail/offer-deatil.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Offer} from '../shared/offer';
 import { OfferService } from '../offer/offer.service';
 import {LoadUserComponent} from '../loadUserInfo/load-user-info.component';
 import { User } from '../shared/user';
+import { OrderService } from '../order-list/order.service';
+import { UserOrder } from '../shared/userOrder';
+import { AlertService } from '../alert-service/alert.service';
 
     
 @Component({
@@ -15,13 +18,15 @@ export class OfferDetailComponent implements OnInit  {
     
      user: User;
      offer: Offer;
-     isInfo: boolean = false
+     isInfo: boolean = false;
+     order: UserOrder;
 
-    constructor(private route: ActivatedRoute, private offerService: OfferService){}
+    constructor(private route: ActivatedRoute, private offerService: OfferService,private ordService: OrderService,private router: Router,private alert: AlertService){}
 
     ngOnInit(){
        var id = +this.route.snapshot.params['id'];
        this.findOffer(id);
+     
     
     }
 
@@ -34,7 +39,37 @@ export class OfferDetailComponent implements OnInit  {
         this.isInfo = !this.isInfo;
     }
 
+    onAdd(){
+       /* this.ordService.getAllOffers().subscribe(res => {
+             let orders = res;
+             if(orders.filter(o => o.workers.find(u => u.id === this.user.id))){ 
+                 
+                this.order = new UserOrder;
+                 this.order.offer = this.offer;                             ЭТФ ХУЙНЯ ДОЛЖНА ДЕЛАТЬ ПРОВЕРКУ СУЩЕСТВУЕТ ЛИ У ЮЗЕРА ОРДЕР, НО ПОЧЕМУ ТА НЕ ПАШЕТ!!!!!!!!!!!!!!!(((((((
+                 let workers = new Array<User>();
+                 workers.push(this.user);
+                this.order.workers = workers;
+                  this.ordService.addOrder(this.order).subscribe(o => this.router.navigateByUrl('/userInfo')  );
+               
+             }
+             else{
+                this.alert.error("Already have this order!");
+             } */
+
+              
+             this.order = new UserOrder;
+             this.order.offer = this.offer;
+             let workers = new Array<User>();
+             workers.push(this.user);
+            this.order.workers = workers;
+              this.ordService.addOrder(this.order).subscribe(o => this.router.navigateByUrl('/userInfo')  );
+        
+
+        }
+  
+               
+    }
+
 
     
 
-}
