e22c330de5ee42df675ca550715bdd3499753312
Artem Slizhevskiy <mykew98@gmail.com>
2018-05-21 21:25:01 +0300

    Fixed category bug when adding new offer

--- src/app/offer-create/offer-create.component.ts
+++ src/app/offer-create/offer-create.component.ts
@@ -9,39 +9,51 @@ import { CategoryService } from '../platform/category.service';
 import { ThrowStmt } from '@angular/compiler';
 
 @Component({
-  selector: 'app-offer-create',
-  templateUrl: './offer-create.component.html',
-  styleUrls: ['./offer-create.component.css']
+    selector: 'app-offer-create',
+    templateUrl: './offer-create.component.html',
+    styleUrls: ['./offer-create.component.css']
 })
 export class OfferCreateComponent implements OnInit {
 
-  categories: Category[];
-  user: User;
-  offer: any = {};
- 
-
-  constructor(private route: ActivatedRoute,private usrServ: UserService, private ofrServ: OfferService, private router: Router,private catServise: CategoryService) { }
-
-  ngOnInit() {
-    var id = +this.route.snapshot.params['id'];
-    this.usrServ.GetUserById(id).subscribe(data => {this.user = data});
-    
-    this.getCategories();
-   
-  }
-
-  private getCategories(){
-
-    this.catServise.getCategories().subscribe(data => this.categories = data);
-    
-  }
+    categories: Category[];
+    user: User;
+    offer: any = {};
+    selectedCategory: string;
 
 
-  onCreate(){
-    this.offer.customer = this.user;
-    this.ofrServ.createOffer(this.offer).subscribe(o => { this.router.navigateByUrl('/userInfo'); });
+    constructor(private route: ActivatedRoute, private usrServ: UserService, private ofrServ: OfferService, private router: Router, private catServise: CategoryService) { }
 
-  }
-  
+    ngOnInit() {
+        var id = +this.route.snapshot.params['id'];
+        this.usrServ.GetUserById(id).subscribe(data => { this.user = data });
+
+        this.getCategories();
+
+    }
+
+    private getCategories() {
+
+        this.catServise.getCategories().subscribe(data => this.categories = data);
+
+    }
+
+    changeSelectedCategory(category: string){
+        this.selectedCategory = category;
+    }
+
+    onCreate() {
+        let catElement = new Category;
+        catElement.name = this.selectedCategory;
+        let catArray = new Array<Category>();
+        this.catServise.getCategories().subscribe(data => {
+            this.categories = data;
+            catArray.push(this.categories.find(exp => exp.name == catElement.name));
+            this.offer.customer = this.user;
+            this.offer.categories = catArray;
+            this.ofrServ.createOffer(this.offer).subscribe(o => { this.router.navigateByUrl('/userInfo'); });
+        })
+
+    }
+
 
 }
