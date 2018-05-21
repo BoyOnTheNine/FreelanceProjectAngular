e55a83f0c822ec2e61fb26c4aff86cc9bd019654
DESKTOP-484ARDL\matvej <matvejk97@gmail.com>
2018-05-21 13:04:37 +0300

    dd filtr offers by category

--- src/app/catalog-info/category-info.component.ts
+++ src/app/catalog-info/category-info.component.ts
@@ -15,7 +15,7 @@ import { CategoryService } from '../platform/category.service';
 export class CategoryInfoComponent implements OnInit  { 
     p: Number = 1;
     
-    objOffers: Offer[] = [];
+    objOffers: Offer[];
     categoryName: string;
 
     constructor(private route: ActivatedRoute, 
@@ -25,17 +25,15 @@ export class CategoryInfoComponent implements OnInit  {
     ngOnInit(){
        var id = +this.route.snapshot.params['id'];
        this.offService.getAllOffers()
-        .subscribe(offers => {this.objOffers = offers;
-            console.log('this.objOffers = ', this.objOffers);
-        });
+       this.getOffer(id);
         this.getCatName(id);
     }
 
     getOffer(Id: number){
-        this.offService.getAllOffers()
-        .subscribe(offers => {this.objOffers = offers;
-            console.log('this.objOffers = ', this.objOffers);
-        });
+        this.offService.getAllOffers().subscribe(data =>{
+            let offs = data;
+            this.objOffers = offs.filter(o => o.categories.find(c =>c.id === Id));
+        } )
     }
 
     getCatName(Id: number){
